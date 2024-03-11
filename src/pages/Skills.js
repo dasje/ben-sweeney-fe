cc914a58ef8a316fbe2c48d8352319a509bf7ae7
Ben <kwalijkje@live.com>
2024-03-11 11:13:51 +0100

    Add skills component

---
+++ src/pages/Skills.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import details from '../details';
+import programmingLanguages from '../skillColors';
+import Card from "../components/Card";
+import ProgressBar from "../components/ProgressBar";
+
+const Skills = () => {
+    return (
+        <Card
+            height="40%"
+            width="50%"
+            title="Programming Skills"
+        >
+            {details.programmingLanguageSkills.map((v,k) => {
+            return <ProgressBar
+                key={k}
+                skillName={v.language}
+                skillColor={programmingLanguages[v.language]}
+                completed={v.proficiency}
+            />
+            })}
+        </Card>
+    )
+};
+
+export default Skills;
