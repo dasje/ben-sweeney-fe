713189c20c3ac9e4e691d16514c6a06abb110e38
Ben <kwalijkje@live.com>
2024-03-11 11:13:05 +0100

    Add about component

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import details from '../details';
+import Card from "../components/Card";
+
+const About = () => {
+    const aboutText = {
+        backgroundColor: "#fb8b24",
+        padding: 10,
+        borderRadius: 'inherit',
+        color: "white",
+    }
+
+    const containerStyles = {
+        backgroundColor: "#e0e0de",
+        borderRadius: 10,
+        margin: 20,
+        padding: 5,
+      }
+
+    return (
+        <Card
+            height="40%"
+            width="50%"
+            title="About"
+        >
+            <div style={containerStyles}>
+                <div style={aboutText}>
+                    {details.aboutText}
+                </div>
+            </div>
+            
+        </Card>
+    )
+};
+
+export default About;
