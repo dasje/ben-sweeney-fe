108939806f6afd311623653e1cd9069d10947b4c
Ben <kwalijkje@live.com>
2024-03-11 11:11:57 +0100

    Add colour palette and personal details json

---
+++ src/skillColors.js
@@ -0,0 +1,16 @@
+const programmingLanguages = {
+        "Java": "#ff0000",
+        "Python": "#ff8700",
+        "Javascript": "#ffd300",
+        "Golang": "#deff0a",
+        "Docker": "#a1ff0a",
+        "Kubernetes": "#0aff99",
+        "Kotlin": "#0aefff"
+    }
+    
+export default programmingLanguages;
+
+// "#0aefff"
+// "#147df5"
+// "#580aff"
+// "#be0aff"
