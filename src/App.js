915b5dd02c801706adaec23c9249a39198467a4b
Ben <kwalijkje@live.com>
2024-03-12 12:41:51 +0100

    Added header buttons and refs to help scrolling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './App.css';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
@@ -9,19 +9,58 @@ import Projects from './pages/Projects';
 import Background from './components/Background';
 import Achievements from './pages/Achievements';
 import Footer from './components/Footer';
+import Landing from "./components/Landing";
+import Button from "./components/Button";
+import EncryptButton from "./components/ButtonEncrypt";
+
+
 
 const App = () => {
+
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const achievementsRef = useRef(null);
+  const projectsRef = useRef(null);
+
+  const goToAbout = () => {
+    aboutRef.current?.scrollIntoView({behavior: 'smooth'});
+    // setBgColor("#5f0f40")
+  };
+  const goToSkills = () => {
+    skillsRef.current?.scrollIntoView({behavior: 'smooth'});
+  };
+  const goToAchievements = () => {
+    achievementsRef.current?.scrollIntoView({behavior: 'smooth'});
+  };
+  const goToProjects = () => {
+    projectsRef.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
     <div className='chau-philomene'>
       <div id='particles-js'>
         <Background />
       </div>
+      <Landing />
+      <Header>
+        <EncryptButton textToDisplay="About" click={goToAbout} />
+        <EncryptButton textToDisplay="Skills" click={goToSkills} />
+        <EncryptButton textToDisplay="Experience" click={goToAchievements} />
+        <EncryptButton textToDisplay="Projects" click={goToProjects} />
+      </Header>
       <ParallaxProvider>
-        <Header />
-        <About />
-        <Skills />
-        <Achievements />
-        <Projects />
+        <div ref={aboutRef}>
+          <About />
+        </div>
+        <div ref={skillsRef}>
+          <Skills />
+        </div>
+        <div ref={achievementsRef}>
+          <Achievements />
+        </div>
+        <div ref={projectsRef}>
+          <Projects />
+        </div>
       </ParallaxProvider>
       <Footer />
     </div>
