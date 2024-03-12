35a82b2ac593f607c38d2c03d8efcf9eaef231cc
Ben <kwalijkje@live.com>
2024-03-12 12:43:07 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,50 @@
-import React, {useState} from "react";
+import React from "react";
+import Card from "./Card";
 
-const Header = () => {
-    const [width, setWidth] = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
-    return <div style={{width: width, height: height}}/>};
+const Header = (props) => {
+    const containerStyles = {
+        backgroundColor: "#e0e0de",
+        borderRadius: 10,
+        padding: 5,
+        
+      }
+    const contentStyle = {
+        backgroundColor: "#9a031e",
+        borderStyle: "solid",
+        border: 100,
+        borderRadius: 10,
+        paddingLeft: 20,
+        paddingBottom: 10,
+        paddingRight: 20,
+        paddingTop: 5,
+        color: "white",
+    }
+
+    return (
+    <div style={{
+        width: '100%',
+        position: 'fixed',
+        top: 0,
+        zIndex: 1
+    }}>
+        <Card
+                height="40%"
+                width="95%"
+        >
+            <div style={containerStyles}>
+                <div style={contentStyle}>
+                    <h1 style={{margin: 'auto', textAlign: 'center', padding: 10}}>
+                        Ben Sweeney
+                    </h1>
+                    <div style={{margin: 'auto', display: 'flex', width: '50%'}}>
+                        {props.children}
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </Card>
+    </div>
+)};
 
 export default Header;
