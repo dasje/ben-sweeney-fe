857e9e70a6d8cc26400e0b208ef4beaf59b2db5e
Ben <kwalijkje@live.com>
2024-03-12 14:38:55 +0100

    Prevent parallax wrapper from stopping scroll behind header element

--- src/pages/ParallaxWrapper.js
+++ src/pages/ParallaxWrapper.js
@@ -4,10 +4,10 @@ import { Parallax } from "react-scroll-parallax";
 const ParallaxWrapper = (props) => {
 
     return (
-        <div style={{width: '99vw', height: '99vh'}}>
+        <div style={{paddingTop: '10%', width: '99vw', height: '99vh'}}>
             <Parallax
                 translateX={[0, 0]}
-                translateY={[0, 100]}
+                translateY={[0, 150]}
                 rotateZ={['turn']}
                 scale={[2, 0.01]}
                 rotate={[0, 0]}
